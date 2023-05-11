7331bb637c1f800ddec5a6e5bf6651a7ff7f403e
Miroslava <117794291+mosorio1@users.noreply.github.com>
2023-05-11 05:28:15 +0000

    new edits

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,55 +4,35 @@ class TodosController < ApplicationController
 
     @list_of_todos = matching_todos.order({ :created_at => :desc })
 
-    render({ :template => "todos/index.html.erb" })
+    #render({ :template => "todos/index.html.erb" })
   end
 
   def show
     the_id = params.fetch("path_id")
-
-    matching_todos = Todo.where({ :id => the_id })
-
-    @the_todo = matching_todos.at(0)
-
+    @the_todo = Todo.where({ :id => the_id })
     render({ :template => "todos/show.html.erb" })
   end
 
   def create
-    the_todo = Todo.new
-    the_todo.content = params.fetch("query_content")
-    the_todo.status = params.fetch("query_status")
-    the_todo.user_id = params.fetch("query_user_id")
-
-    if the_todo.valid?
-      the_todo.save
-      redirect_to("/todos", { :notice => "Todo created successfully." })
-    else
-      redirect_to("/todos", { :alert => the_todo.errors.full_messages.to_sentence })
-    end
+    @the_todo = Todo.new
+    @the_todo.content = params.fetch("query_content")
+    @the_todo.user_id = params.fetch("query_user_id")
+    @the_todo.save
+    redirect_to("/")
   end
 
   def update
-    the_id = params.fetch("path_id")
-    the_todo = Todo.where({ :id => the_id }).at(0)
-
-    the_todo.content = params.fetch("query_content")
-    the_todo.status = params.fetch("query_status")
-    the_todo.user_id = params.fetch("query_user_id")
-
-    if the_todo.valid?
-      the_todo.save
-      redirect_to("/todos/#{the_todo.id}", { :notice => "Todo updated successfully."} )
-    else
-      redirect_to("/todos/#{the_todo.id}", { :alert => the_todo.errors.full_messages.to_sentence })
-    end
+    @todo = Todo.find(params[:path_id])
+    updated_status = params.fetch("query_status")
+    @todo.status = updated_status
+    @todo.save   
+    redirect_to("/")
   end
 
   def destroy
     the_id = params.fetch("path_id")
     the_todo = Todo.where({ :id => the_id }).at(0)
-
     the_todo.destroy
-
-    redirect_to("/todos", { :notice => "Todo deleted successfully."} )
+    redirect_to("/")
   end
 end
